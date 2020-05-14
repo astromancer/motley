98286078d649aaa5a034e7c16d6d5f8b5b872930
astromancer <hannes@saao.ac.za>
2020-05-14 20:51:12 +0200

    test improvements

--- tests/test_table.py
+++ tests/test_table.py
@@ -18,33 +18,76 @@ def random_words(word_size, n_words, ord_range=(97, 122)):
 
 # TODO: loads more basic tests to showcase functionality
 
+# TODO: automated way of looping through all possible argument combinations
+#  that make sense??
+
+
+# test_simple
 data = np.random.randn(10, 10)
-tbl = Table(data)
-print(tbl)
+tp = (dict(txt=('bold', 'm'), bg='g'))
 
-Table(data,
-      title='Random data',
-      precision=3,
-      title_props=dict(txt=('bold', 'm'), bg='g'))
+tables = [
+    # test_simple
+    Table(data),
 
-Table(data,
-      title='Random data',
-      precision=3,
-      title_props=(dict(txt=('bold', 'm'), bg='g')),
-      totals=True,
-      row_nrs=True,
-      col_headers=random_words(5, 10),
-      col_head_props=('italic', 'y'))
+    # test title
+    Table(data,
+          title='Random data',
+          precision=3,
+          title_props=tp),
 
-Table.from_columns(*np.random.randn(3, 10),
-                   *np.random.randint(0, 1000, (3, 10)),
-                   title='Mixed float, int',
-                   precision=3, minimalist=True,)
-                   #title_props=(dict(txt=('bold', 'm'), bg='g')),
-                   #total=True,
-                   #row_nrs=True,
-                   #col_headers=random_words(5, 10),
-                   #col_head_props=('italic', 'y'))
+    # test col_headers
+    Table(data,
+          title='Random data',
+          precision=3,
+          title_props=tp,
+          total=True,
+          row_nrs=True,
+          col_headers=random_words(5, 10),
+          col_head_props=('italic', 'y')),
+
+    # test terse keywords
+    Table(data,
+          title='Random data',
+          nrs=True,
+          chead=random_words(5, 10),
+          chead_prop=('italic', 'y')),
+
+    # test minimalist
+    Table.from_columns(*np.random.randn(3, 10),
+                       *np.random.randint(0, 1000, (3, 10)),
+                       title='Mixed float, int',
+                       precision=3, minimalist=True, ),
+
+    # test auto alignment
+    # test compactify
+    # test auto format
+    # test highlight
+    # test multiline cells
+    # test ansi input
+
+    Table(data,
+          title='Random data',
+          precision=3,
+          title_props=(dict(txt=('bold', 'm'), bg='g')),
+          totals=True,
+          row_nrs=True,
+          col_headers=random_words(5, 10),
+          col_head_props=('italic', 'y')),
+
+    Table.from_columns(*np.random.randn(3, 10),
+                       *np.random.randint(0, 1000, (3, 10)),
+                       title='Mixed float, int',
+                       precision=3, minimalist=True, ),
+    # title_props=(dict(txt=('bold', 'm'), bg='g')),
+    # total=True,
+    # row_nrs=True,
+    # col_headers=random_words(5, 10),
+    # col_head_props=('italic', 'y'))
+]
+
+for tbl in tables:
+    print(tbl)
 
 # # from dict
 # Ncpus = 8
@@ -55,12 +98,3 @@ Table.from_columns(*np.random.randn(3, 10),
 #                     ('defer', 2))
 # table = Table(title='Load balance', data=nproc, order='r')
 # print(table)
-
-# test_simple
-# test_alignment
-# test auto alignment
-# test compactify
-# test auto format
-# test highlight
-# test multiline cells
-# test ansi input
