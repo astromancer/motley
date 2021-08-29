c71761eb97e1fab30ff2d83d8dd7efdbfdad7bf4
astromancer <hannes@saao.ac.za>
2021-08-29 19:07:52 +0200

    improved color resolution

--- src/motley/codes.py
+++ src/motley/codes.py
@@ -9,7 +9,7 @@ import functools as ftl
 
 # third-party libs
 import numpy as np
-from matplotlib.colors import to_rgb
+import more_itertools as mit
 
 # local libs
 from recipes.dicts import ManyToOneMap
@@ -17,6 +17,7 @@ from recipes.string import replace_prefix
 
 # relative libs
 from .ansi import parse
+from .colors import CSS_TO_RGB
 
 
 # see:  https://en.wikipedia.org/wiki/ANSI_escape_code
@@ -29,6 +30,7 @@ END = CSI + '0m'
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ANSI Codes for Text effects and colours  FG_CODES BG_CODE
+#
 FG_CODES = {
     'bold': 1,
     'dim': 2,                   # faint
@@ -133,16 +135,16 @@ BG_CODES = {
 
 
 # Short colour descriptions
-# mplShortsMap = {
-#     'b': 'blue',
-#     'g': 'green',
-#     'r': 'red',
-#     'c': 'cyan',
-#     'm': 'magenta',
-#     'y': 'yellow',
-#     'k': 'black',
-#     'w': 'white',
-# }
+colorAliasMap = {
+    'r': 'red',
+    'b': 'blue',
+    'g': 'green',
+    'c': 'cyan',
+    'm': 'magenta',
+    'y': 'yellow',
+    'k': 'black',
+    'w': 'white',
+}
 
 #
 effectAliasMap = {
@@ -192,61 +194,57 @@ kwAliasMap = {
 # def _aliasFactory(codes, aliases):
 #     """Create the code translation dict"""
 #     Codes = ManyToOneMap(codes)
-#     Codes.add_vocab(aliases)
+#     Codes.add_mapping(aliases)
 #     Codes.add_map(str.lower)
 #     return Codes
 
-
-class KeyMap(ManyToOneMap):
-    """
-    Resolve all the various ways in which colours or effects can be specified.
-    """
-
-    def __init__(self, dic=None, **kws):
-        super().__init__(dic, **kws)
-        self.add_vocab(kwAliasMap)  # translation
+class ResolverBase(ManyToOneMap):
+    template = ''
 
     def __missing__(self, key):
         try:
             return super().__missing__(key)
         except KeyError:
-            pass
-        raise KeyError(
-            f'{key!r} is not a valid description for text or background effect.'
-        )
+            raise KeyError(self.template.format(key)) from None
 
 
-class CodeMap(ManyToOneMap):  # CodeMap
+class KeyMap(ResolverBase):
+    """
+    Resolve all the various ways in which colours or effects can be specified.
+    """
+    template = ('{key:r} is not a valid description for a text or '
+                'background effect.')
+
+    def __init__(self, dic=None, **kws):
+        super().__init__(dic, **kws)
+        self.add_mapping(kwAliasMap)  # translation
+
+
+class CodeMap(ResolverBase):
     """
     Resolve all the various names for colours or effects into ansi codes.
     """
 
-    # def __init__(self, dic=None, **kws):
-    #     super().__init__(dic, **kws)
-    # add mappings for matplotlib color names eg: 'r' --> 'red' etc..
-    # self.add_vocab(mplShortsMap)
-    # add a layer that maps to lower case: 'REd' --> 'red'
-    # self.add_mapping(str.lower)
+    def __init__(self, dic=None, **kws):
+        super().__init__(dic, **kws)
+        # add mappings for matplotlib color names eg: 'r' --> 'red' etc..
+        self.add_mapping(colorAliasMap)
+        # add a layer that maps to lower case: 'REd' --> 'red'
+        self.add_func(str.lower)
+        # add light -> bright translation
+        self.add_func(ftl.partial(replace_prefix, old='bright', new='light'))
+
+    template = '{key:r} is not a valid colour or effect.'
 
     def __getitem__(self, key):
         # make sure we always return a str
         return str(super().__getitem__(key))
 
-    def __missing__(self, key):
-        try:
-            return super().__missing__(key)
-        except KeyError:
-            raise KeyError(f'Unknown colour or effect {key!r}') from None
-
 
 # additional shorthands for bold / italic text
 fg_codes, bg_codes = CodeMap(FG_CODES), CodeMap(BG_CODES)
+fg_codes.add_mapping(effectAliasMap)
 
-light_is_bright = ftl.partial(replace_prefix, old='bright', new='light')
-fg_codes.add_mapping(light_is_bright)
-bg_codes.add_mapping(light_is_bright)
-
-fg_codes.add_vocab(effectAliasMap)
 
 # Keyword Translator
 codes = KeyMap(fg=fg_codes, bg=bg_codes)
@@ -260,14 +258,24 @@ FORMAT_24BIT = KeyMap(fg='38;2;{:d};{:d};{:d}',
 # Dispatch functions for translating user input to ANSI codes
 
 
+class InvalidEffect(Exception):
+    """
+    Raised when a user input object cannot be resolved to a code for colour or 
+    effect.
+    """
+
+    def __init__(self, obj, fg_or_bg):
+        super().__init__(
+            (f'Could not interpret object {obj!r} of type {type(obj)!r} as a '
+             f'valid colour or effect for {fg_or_bg!r}')
+        )
+
+
 @ftl.singledispatch
 def resolve(obj, fg_or_bg='fg'):
     """default dispatch func for resolving ANSI codes from user input"""
-    raise TypeError('Could not interpret %r (type %r) as a colour / effect '
-                    'for %r' % (obj, type(obj), fg_or_bg))
+    raise InvalidEffect(obj, fg_or_bg)
 
-
-# TODO: might want to give the functions below names for more readable traceback
 
 @resolve.register(type(None))
 def _(obj, fg_or_bg='fg'):
@@ -280,10 +288,24 @@ def _(obj, fg_or_bg='fg'):
     if obj == '':
         return
 
-    try:
-        yield from resolve(to_24bit(obj), fg_or_bg)
-    except ValueError:
-        yield codes[fg_or_bg][obj]
+    # resolve hex / html / css colours
+    if obj.startswith('#'):
+        yield hex_to_rgb(obj)
+        return
+
+    # try resolve as a named color / effect
+    value = codes[fg_or_bg].get(obj, None)
+    if value:
+        yield value
+        return
+
+    # try resolve as a named CSS color
+    value = CSS_TO_RGB.get(obj, None)
+    if value:
+        yield FORMAT_24BIT[fg_or_bg].format(*value)
+        return
+
+    raise InvalidEffect(obj, fg_or_bg)
 
 
 @resolve.register(numbers.Integral)
@@ -292,20 +314,20 @@ def _(obj, fg_or_bg='fg'):
     if 0 <= obj < 256:
         yield FORMAT_8BIT[fg_or_bg].format(obj)
     else:
-        raise ValueError('Could not interpret key %r as a 8 bit colour' % obj)
+        raise ValueError(f'Could not interpret key {obj!r} as a 8 bit colour.')
 
 
 @resolve.register(np.ndarray)
 @resolve.register(list)
 @resolve.register(tuple)
 def _(obj, fg_or_bg='fg'):
-    # tuple, lists are interpreted as 24-bit rgb true colour codes
+    # tuple, lists are interpreted as 24-bit rgb colour codes
     if is_24bit(obj):
         if all(0 <= _ < 256 for _ in obj):
             yield FORMAT_24BIT[fg_or_bg].format(*obj)
         else:
             raise ValueError(
-                f'Could not interpret key {obj!r} as a 24 bit colour'
+                f'Could not interpret key {obj!r} as a 24 bit colour.'
             )
     else:
         for p in obj:
@@ -327,11 +349,22 @@ def is_24bit(obj):
     return all(isinstance(o, numbers.Integral) for o in obj)
 
 
-def to_24bit(name):
-    return tuple((np.multiply(to_rgb(name), 255).astype(int)))
+# def to_24bit(name):
+#     #
+#     return tuple((np.multiply(to_rgb(name), 255).astype(int)))
+
+# to_rgb = to_24bit
 
 
-def _gen_codes(*effects, **kws):
+def hex_to_rgb(value):
+    """#000080 -> (0, 0, 128)"""
+    value = value.lstrip('#')
+    size = len(value)
+    assert size in {3, 6}
+    return tuple(int(bit, 16) for bit in mit.sliced(value, size // 3))
+
+
+def _iter_codes(*effects, **kws):
     """
 
     Parameters
@@ -348,9 +381,13 @@ def _gen_codes(*effects, **kws):
     yield from resolve(kws)
 
 
-def _get_params(*effects, **kws): # TODO better name
-    # get the nrs '34;48;5;22' part of the code
-    return ';'.join(_gen_codes(*effects, **kws))
+def get_code_list(*effects, **kws):
+    return list(_iter_codes(*effects, **kws))
+
+
+def get_code_str(*effects, **kws):
+    # get the semi-colon separated integers as a string: eg '34;48;5;22'
+    return ';'.join(_iter_codes(*effects, **kws))
 
 
 def get(*effects, **kws):
@@ -366,7 +403,7 @@ def get(*effects, **kws):
     -------
 
     """
-    return ''.join((CSI, _get_params(*effects, **kws), 'm'))
+    return ''.join((CSI, get_code_str(*effects, **kws), 'm'))
 
 
 def from_list(fg=None, bg=None):
@@ -397,7 +434,7 @@ def apply(s, *effects, **kws):
     # string = str(s)
 
     # get code bits eg: '34;48;5;22'
-    new_codes = _get_params(*effects, **kws)
+    new_codes = get_code_str(*effects, **kws)
     if not new_codes:
         return s
 
@@ -416,7 +453,6 @@ def apply(s, *effects, **kws):
 
     return ''.join(f'{CSI}{params};{new_codes}m{w}{END}'
                    for _, params, _, w, _ in parse(s))
-    
 
 
 def apply_naive(s, *effects, **kws):
